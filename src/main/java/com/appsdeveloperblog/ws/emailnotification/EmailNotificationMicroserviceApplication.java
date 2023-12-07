20b0c0a98866d76ef831f5c184fe7136ff650f5f
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-12-07 12:02:38 -0500

    Initial commit

---
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/EmailNotificationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.ws.emailnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailNotificationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailNotificationMicroserviceApplication.class, args);
+	}
+
+}
