2eb78480471992697da6e4e7260a184589fd008c
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-12-15 18:01:18 -0500

    Throw a NotRetryableException

--- src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
@@ -7,6 +7,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.appsdeveloperblog.ws.core.ProductCreatedEvent;
+import com.appsdeveloperblog.ws.emailnotification.error.NotRetryableException;
 
 @Component
 @KafkaListener(topics="product-created-events-topic")
@@ -16,6 +17,7 @@ public class ProductCreatedEventHandler {
 
 	@KafkaHandler
 	public void handle(ProductCreatedEvent productCreatedEvent) {
+		if(true) throw new NotRetryableException("An error took place. No need to consume this message again.");
 		LOGGER.info("Received a new event: " + productCreatedEvent.getTitle());
 	}
 	
