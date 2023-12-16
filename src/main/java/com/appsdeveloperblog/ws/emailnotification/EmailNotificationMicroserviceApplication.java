497e2c83986dd5e04db451e83b270b62825efa52
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-12-16 14:54:58 -0500

    Throw RetryableException from the handle() method

--- src/main/java/com/appsdeveloperblog/ws/emailnotification/EmailNotificationMicroserviceApplication.java
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/EmailNotificationMicroserviceApplication.java
@@ -2,6 +2,8 @@ package com.appsdeveloperblog.ws.emailnotification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EmailNotificationMicroserviceApplication {
@@ -10,4 +12,9 @@ public class EmailNotificationMicroserviceApplication {
 		SpringApplication.run(EmailNotificationMicroserviceApplication.class, args);
 	}
 
+	@Bean
+	RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
 }
--- src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
@@ -2,23 +2,54 @@ package com.appsdeveloperblog.ws.emailnotification.handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 
 import com.appsdeveloperblog.ws.core.ProductCreatedEvent;
 import com.appsdeveloperblog.ws.emailnotification.error.NotRetryableException;
+import com.appsdeveloperblog.ws.emailnotification.error.RetryableException;
 
 @Component
-@KafkaListener(topics="product-created-events-topic")
+@KafkaListener(topics = "product-created-events-topic")
 public class ProductCreatedEventHandler {
-	
+
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	private RestTemplate restTemplate;
+
+	public ProductCreatedEventHandler(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
 	@KafkaHandler
 	public void handle(ProductCreatedEvent productCreatedEvent) {
-		if(true) throw new NotRetryableException("An error took place. No need to consume this message again.");
 		LOGGER.info("Received a new event: " + productCreatedEvent.getTitle());
+
+		String requestUrl = "http://localhost:8082";
+
+		try {
+			ResponseEntity<String> response = restTemplate.exchange(requestUrl, HttpMethod.GET, null, String.class);
+
+			if (response.getStatusCode().value() == HttpStatus.OK.value()) {
+				LOGGER.info("Received response from a remote service: " + response.getBody());
+			}
+		} catch (ResourceAccessException ex) {
+			LOGGER.error(ex.getMessage());
+			throw new RetryableException(ex);
+		} catch(HttpServerErrorException ex) {
+			LOGGER.error(ex.getMessage());
+			throw new NotRetryableException(ex);
+		} catch(Exception ex) {
+			LOGGER.error(ex.getMessage());
+			throw new NotRetryableException(ex);
+		}
+
 	}
-	
+
 }
